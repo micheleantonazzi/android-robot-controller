3c669ae43269b4891ec3423fc8ed2858d6682756
micheleantonazzi <micheleantonazzi@gmail.com>
2019-12-18 15:10:41 +0100

    draw green rectangle

--- android_robot_controller/src/main/java/org/ros/android/android_robot_controller/OpenGL/Renderes/MapRenderer.java
+++ android_robot_controller/src/main/java/org/ros/android/android_robot_controller/OpenGL/Renderes/MapRenderer.java
@@ -3,15 +3,36 @@ package org.ros.android.android_robot_controller.OpenGL.Renderes;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 
+import org.ros.android.android_robot_controller.Rectangle;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class MapRenderer implements GLSurfaceView.Renderer {
 
+    Rectangle rectangle;
+
+    // This method compiles the OpenGL Shading Language
+    // Create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+    // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+    public static int loadShader(int type, String shaderCode){
+
+        // Create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
+    }
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         GLES20.glClearColor(1, 0, 0, 1);
+
+        this.rectangle = new Rectangle();
     }
 
     @Override
@@ -21,6 +42,6 @@ public class MapRenderer implements GLSurfaceView.Renderer {
 
     @Override
     public void onDrawFrame(GL10 gl) {
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        this.rectangle.draw();
     }
 }
