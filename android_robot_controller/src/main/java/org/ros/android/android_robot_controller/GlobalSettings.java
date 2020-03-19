923b3fe5d2fcc9cba468d06f2cc8d4cbecce916d
micheleantonazzi <micheleantonazzi@gmail.com>
2020-03-19 17:10:58 +0100

    update GlobalSettings

--- android_robot_controller/src/main/java/org/ros/android/android_robot_controller/GlobalSettings.java
+++ android_robot_controller/src/main/java/org/ros/android/android_robot_controller/GlobalSettings.java
@@ -9,8 +9,8 @@ public class GlobalSettings {
     // Preferences strings
     public static final String PREFERENCES_SETTINGS = "preferences_settings";
 
-    public static final String PREFERENCES_NAMESPACE = "preference_settings_namespace";
-    private static final String PREFERENCES_NAMESPACE_DEFAULT = "android_robot_controller/";
+    public static final String PREFERENCE_NAMESPACE = "preference_settings_namespace";
+    private static final String PREFERENCE_NAMESPACE_DEFAULT = "android_robot_controller/";
 
     public static final String PREFERENCE_MAP_TOPIC = "preference_setting_map_topic";
     private static final String PREFERENCE_MAP_TOPIC_DEFAULT = "/map";
@@ -42,27 +42,117 @@ public class GlobalSettings {
 
     public String getPreferenceFromName(String preferenceName){
         switch (preferenceName){
-            case PREFERENCES_NAMESPACE:
+            case PREFERENCE_NAMESPACE:
                 return this.getApplicationNamespace();
+            case PREFERENCE_MAP_TOPIC:
+                return this.getMapTopic();
+            case PREFERENCE_MAP_METADATA_TOPIC:
+                return this.getMapMetadataTopic();
+            case PREFERENCE_TF_TOPIC:
+                return this.getTFTopic();
+            case PREFERENCE_GOAL_TOPIC:
+                return this.getGoalTopic();
+            case PREFERENCE_CAMERA_COMPRESS_TOPIC:
+                return this.getCameraCompressedTopic();
+            case PREFERENCE_CONTROL_TOPIC:
+                return this.getControlTopic();
         }
         return "";
     }
 
     public void setPreferenceFromName(String preferenceName, String value){
         switch (preferenceName){
-            case PREFERENCES_NAMESPACE:
+            case PREFERENCE_NAMESPACE:
                 this.setApplicationNamespace(value);
+                break;
+            case PREFERENCE_MAP_TOPIC:
+                this.setMapTopic(value);
+                break;
+            case PREFERENCE_MAP_METADATA_TOPIC:
+                this.setMapMetadataTopic(value);
+                break;
+            case PREFERENCE_TF_TOPIC:
+                this.setTFTopic(value);
+                break;
+            case PREFERENCE_GOAL_TOPIC:
+                this.setGoalTopic(value);
+                break;
+            case PREFERENCE_CAMERA_COMPRESS_TOPIC:
+                this.setCameraCompressedTopic(value);
+                break;
+            case PREFERENCE_CONTROL_TOPIC:
+                this.setControlTopic(value);
                 break;
         }
     }
 
     public String getApplicationNamespace(){
         return this.preferencesSettings != null ?
-                this.preferencesSettings.getString(PREFERENCES_NAMESPACE, PREFERENCES_NAMESPACE_DEFAULT)
+                this.preferencesSettings.getString(PREFERENCE_NAMESPACE, PREFERENCE_NAMESPACE_DEFAULT)
                 : "";
     }
 
     public void setApplicationNamespace(String applicationNamespace){
-        this.preferencesSettings.edit().putString(PREFERENCES_NAMESPACE, applicationNamespace).commit();
+        this.preferencesSettings.edit().putString(PREFERENCE_NAMESPACE, applicationNamespace).apply();
+    }
+
+    public String getMapTopic(){
+        return this.preferencesSettings != null ?
+                this.preferencesSettings.getString(PREFERENCE_MAP_TOPIC, PREFERENCE_MAP_TOPIC_DEFAULT)
+                : "";
+    }
+
+    public void setMapTopic(String mapTopic){
+        this.preferencesSettings.edit().putString(PREFERENCE_MAP_TOPIC, mapTopic).apply();
+    }
+
+    public String getMapMetadataTopic(){
+        return this.preferencesSettings != null ?
+                this.preferencesSettings.getString(PREFERENCE_MAP_METADATA_TOPIC, PREFERENCE_MAP_METADATA_TOPIC_DEFAULT)
+                : "";
+    }
+
+    public void setMapMetadataTopic(String mapMetadataTopic){
+        this.preferencesSettings.edit().putString(PREFERENCE_MAP_METADATA_TOPIC, mapMetadataTopic).apply();
+    }
+
+    public String getTFTopic(){
+        return this.preferencesSettings != null ?
+                this.preferencesSettings.getString(PREFERENCE_TF_TOPIC, PREFERENCE_TF_TOPIC_DEFAULT)
+                : "";
+    }
+
+    public void setTFTopic(String tfTopic){
+        this.preferencesSettings.edit().putString(PREFERENCE_TF_TOPIC, tfTopic).apply();
+    }
+
+    public String getGoalTopic(){
+        return this.preferencesSettings != null ?
+                this.preferencesSettings.getString(PREFERENCE_GOAL_TOPIC, PREFERENCE_GOAL_TOPIC_DEFAULT)
+                : "";
+    }
+
+    public void setGoalTopic(String goalTopic){
+        this.preferencesSettings.edit().putString(PREFERENCE_TF_TOPIC, goalTopic).apply();
+    }
+
+    public String getCameraCompressedTopic(){
+        return this.preferencesSettings != null ?
+                this.preferencesSettings.getString(PREFERENCE_CAMERA_COMPRESS_TOPIC, PREFERENCE_CAMERA_COMPRESS_TOPIC_DEFAULT)
+                : "";
+    }
+
+    public void setCameraCompressedTopic(String cameraCompressedTopic){
+        this.preferencesSettings.edit().putString(PREFERENCE_CAMERA_COMPRESS_TOPIC, cameraCompressedTopic).apply();
+    }
+
+    public String getControlTopic(){
+        return this.preferencesSettings != null ?
+                this.preferencesSettings.getString(PREFERENCE_CONTROL_TOPIC, PREFERENCE_CONTROL_TOPIC_DEFAULT)
+                : "";
+    }
+
+    public void setControlTopic(String controlTopic){
+        this.preferencesSettings.edit().putString(PREFERENCE_CONTROL_TOPIC, controlTopic).apply();
     }
 }
